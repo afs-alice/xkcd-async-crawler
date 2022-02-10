02773a3c95f9ff0685c3ff0762eb8f75f7baca77
Daniel Marcelino Azevedo <daniel.azevedo@justicafacil.com.br>
2022-02-10 17:36:50 -0300

    Cria arquivo run.py

---
+++ run.py
@@ -0,0 +1,15 @@
+import time
+from src.xkcd_async_downloader import XkcdAsyncDownloader
+
+def main() -> None:
+    start = time.perf_counter()
+    instance = XkcdAsyncDownloader()
+    instance.make_download()
+    print('End of execution!')
+    print(f'Resume: {instance.get_amout_of_saved_files}'
+          ' comics image files has been downloaded and saved '
+          f'in {instance.DIRECTORY}/')
+    print('Execution time: {:.2f}s'.format(time.perf_counter() - start))
+
+if __name__ == '__main__':
+    main()
