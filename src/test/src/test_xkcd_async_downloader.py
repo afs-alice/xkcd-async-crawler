564a1ef19ca52f2428a419aa6b80775c0ad8d3e2
Daniel Marcelino Azevedo <daniel.azevedo@justicafacil.com.br>
2022-02-15 02:03:19 -0300

    Adiciona casos de teste

--- src/test/src/test_xkcd_async_downloader.py
+++ src/test/src/test_xkcd_async_downloader.py
@@ -1,5 +1,9 @@
 import asyncio
+from asyncore import read
+from email import header
 from http.client import InvalidURL
+from importlib.resources import contents
+from telnetlib import STATUS
 from time import sleep
 import unittest
 import json
@@ -20,9 +24,10 @@ def async_test(coroutine):
             pass
     return wrapped
 
+
 class MockResponse(AsyncClass):
     async def __ainit__(self, status: int = 200, json: dict = {}, read: bytes = b'',
-                       headers: dict = {}) -> None:
+                        headers: dict = {}) -> None:
         self.status = status
         self.headers = headers
         self.json_content = json
@@ -36,18 +41,25 @@ class MockResponse(AsyncClass):
         sleep(0)
         return self.read_content
 
+
 def get_api_json_fixture() -> dict:
     with open('src/test/src/fixtures/api_content_file.json') as file:
         return json.loads(file.read())
+
 
 def get_headers_img_file_fixture() -> dict:
     with open('src/test/src/fixtures/headers_img_file.json') as file:
         return json.loads(file.read()) 
 
+
 def get_headers_no_img_file_fixture() -> dict:
     with open('src/test/src/fixtures/headers_no_img_file.json') as file:
         return json.loads(file.read()) 
 
+
+def get_img_file_binary_content() -> bytes:
+    with open('src/test/src/fixtures/img_file.png', 'rb') as file:
+        return file.read()
 
 class TestCreateDirectory(unittest.TestCase):
     def setUp(self) -> None:
@@ -99,11 +111,7 @@ class TestSaveFileInLocalStorage(unittest.TestCase):
     def setUp(self) -> None:
         self.instance = XkcdAsyncDownloader()
         self.instance.DIRECTORY = '.'
-        with open('src/test/src/fixtures/img_file.png', 'rb') as img_file:
-            self.file_data = {
-                'content': img_file.read(),
-                'extension': 'png'
-            }
+        self.file_data = {'content': get_img_file_binary_content(), 'extension': 'png'}
         self.comic_id = 2579
         self.md5_img_name_file = '2f00e94162e4023b0270a3f309588075.png'
         self.file_path = f'{self.instance.DIRECTORY}/{self.md5_img_name_file}'
@@ -124,7 +132,6 @@ class TestSaveFileInLocalStorage(unittest.TestCase):
         self.assertEqual(captured_log.output[0], f'INFO:root:Comic id: {self.comic_id} has been saved with '
                                                  f'name: {self.md5_img_name_file}')
         self.assertEqual(1, self.instance._XkcdAsyncDownloader__count_of_saved_files)
-
 
     @patch('os.path.isfile', return_value=True)
     @async_test
@@ -159,15 +166,6 @@ class TestGetLastIndex(unittest.TestCase):
     
     @patch('aiohttp.client.ClientSession.request')
     @async_test
-    async def test_return_last_index_and_display_info_log(self, mock_iorequest):
-        mock_iorequest.return_value = MockResponse(json = get_api_json_fixture())
-        with self.assertLogs() as captured_log:
-            method_return = await self.instance._XkcdAsyncDownloader__get_last_index(ClientSession())
-        self.assertEqual(captured_log.output[0],f'INFO:root:Last comic index (comic id): {method_return}')
-        self.assertEqual(self.expected_last_index, method_return)
-    
-    @patch('aiohttp.client.ClientSession.request')
-    @async_test
     async def test_return_false_and_display_error_log_when_exceptions_are_raised(self, mock_iorequest):
         known_exceptions = [InvalidURL, TimeoutError, ConnectionError]
         for exception in known_exceptions:
@@ -182,15 +180,146 @@ class TestGetLastIndex(unittest.TestCase):
     @patch('aiohttp.client.ClientSession.request')
     @async_test
     async def test_return_false_and_display_error_log_when_status_code_is_not_200(self, mock_iorequest):
-        mock_iorequest.return_value = MockResponse(status=404)
+        status_codes = [403, 404, 500]
+        for code in status_codes:
+            mock_iorequest.return_value = MockResponse(status=code)
+            with self.assertLogs() as captured_log:
+                method_return = await self.instance._XkcdAsyncDownloader__get_last_index(ClientSession())
+            self.assertFalse(method_return)
+            self.assertEqual(
+                captured_log.output[0], f'ERROR:root:Error {code} when getting last comic index from xkcd API'
+            )
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_return_last_index_and_display_info_log(self, mock_iorequest):
+        mock_iorequest.return_value = MockResponse(json = get_api_json_fixture())
         with self.assertLogs() as captured_log:
             method_return = await self.instance._XkcdAsyncDownloader__get_last_index(ClientSession())
-        self.assertFalse(method_return)
-        self.assertEqual(captured_log.output[0],f'ERROR:root:Error {mock_iorequest.return_value.status} '
-                                                'when getting last comic '
-                                                'index from xkcd API')
-
+        self.assertEqual(captured_log.output[0],f'INFO:root:Last comic index (comic id): {method_return}')
+        self.assertEqual(self.expected_last_index, method_return)
     
+
+class TestGetComicImageUrl(unittest.TestCase):
+    def setUp(self) -> None:
+        self.instance = XkcdAsyncDownloader()
+        self.image_url = 'https://imgs.xkcd.com/comics/tractor_beam.png'
+        self.comic_id = 2579
+        self.comic_title = 'Tractor Beam'
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_display_warning_log_and_return_false_when_status_code_is_not_200(self, mock_iorequest):
+        status_codes = [403, 404, 500]
+        for code in status_codes:
+            mock_iorequest.return_value = MockResponse(status=code)
+            with self.assertLogs() as captured_log:
+                method_return = await self.instance._XkcdAsyncDownloader__get_comic_image_url(
+                    self.comic_id, ClientSession()
+                )
+            self.assertEqual(
+                captured_log.output[0],
+                f'WARNING:root:Error {code} in request for comic id image file: {self.comic_id}'
+            )
+            self.assertFalse(method_return)  
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_display_error_log_and_return_false_when_exceptions_are_raised(self, mock_iorequest):
+        known_exceptions = [InvalidURL, TimeoutError, ConnectionError]
+        for exception in known_exceptions:
+            mock_iorequest.side_effect = exception
+            with self.assertLogs() as captured_log:
+                method_return = await self.instance._XkcdAsyncDownloader__get_comic_image_url(
+                    self.comic_id, ClientSession()
+                )
+            self.assertFalse(method_return)
+            self.assertEqual(
+                captured_log.output[0],
+                f'WARNING:root:Error {exception.__name__} in request for comic id image file: {self.comic_id}'
+            )
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_return_image_url_and_info_log(self, mock_iorequest):
+        mock_iorequest.return_value = MockResponse(json = get_api_json_fixture())
+        with self.assertLogs() as captured_log:
+            method_return = await self.instance._XkcdAsyncDownloader__get_comic_image_url(
+                self.comic_id, ClientSession()
+            )
+        self.assertEqual(
+            captured_log.output[0],
+            f'INFO:root:URL from image comic id: {self.comic_id}, title: {self.comic_title}, '
+            'has been obtained from API'
+        )
+        self.assertEqual(self.image_url, method_return)
+
+
+class TestGetImageFile(unittest.TestCase):
+    def setUp(self) -> None:
+        self.instance = XkcdAsyncDownloader()
+        self.img_file_content = get_img_file_binary_content()
+        self.comic_id = 2579
+        self.image_url = 'https://imgs.xkcd.com/comics/tractor_beam.png'
+        self.image_file_data = {'content': get_img_file_binary_content(), 'extension': 'png'}
+        self.headers = get_headers_img_file_fixture()
+    
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_display_warning_log_and_return_false_when_exceptions_are_raised(self, mock_iorequest):
+        known_exceptions = [InvalidURL, TimeoutError, ConnectionError]
+        for exception in known_exceptions:
+            mock_iorequest.side_effect = exception
+            with self.assertLogs() as captured_log:
+                method_return = await self.instance._XkcdAsyncDownloader__get_image_file(
+                    self.comic_id, self.image_url, ClientSession()
+                )
+            self.assertFalse(method_return)
+            self.assertEqual(
+                captured_log.output[0],
+                f'WARNING:root:Error {exception.__name__} in request for comic id image file: {self.comic_id}'
+            )
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_return_false_and_warning_log_when_status_code_is_not_200(self, mock_iorequest):
+        status_codes = [403, 404, 500]
+        for code in status_codes:
+            mock_iorequest.return_value = MockResponse(status=code)
+            with self.assertLogs() as captured_log:
+                method_return = await self.instance._XkcdAsyncDownloader__get_image_file(
+                    self.comic_id, self.image_url, ClientSession()
+                )
+            self.assertEqual(
+                captured_log.output[0],
+                f'WARNING:root:Error {code} in request for comic id image file: {self.comic_id}'
+            )
+            self.assertFalse(method_return)
+    
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def test_return_false_and_warning_log_when_content_of_response_is_not_image(self, mock_iorequest):
+        mock_iorequest.return_value = MockResponse(headers=get_headers_no_img_file_fixture())
+        with self.assertLogs() as captured_log:
+            method_return = await self.instance._XkcdAsyncDownloader__get_image_file(
+                self.comic_id, self.image_url, ClientSession()
+            )
+        self.assertEqual(
+            captured_log.output[0],
+            f'WARNING:root:The file for comic id: {self.comic_id} is not a image'
+        )
+        self.assertFalse(method_return)
+
+    @patch('aiohttp.client.ClientSession.request')
+    @async_test
+    async def teste_return_image_file_data(self, mock_iorequest):
+        mock_iorequest.return_value = MockResponse(read=self.img_file_content, headers=self.headers)
+        method_return = await self.instance._XkcdAsyncDownloader__get_image_file(
+            self.comic_id, self.image_url, ClientSession()
+        )
+        self.assertEqual(self.image_file_data, method_return)
+
 
 if __name__ == '__main__':
     unittest.main()
